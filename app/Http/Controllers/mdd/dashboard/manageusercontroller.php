cf62ba683468462c4b3e543204d272a7d2a3e0b3
jayson <jayson.claros@gmail.com>
2023-03-29 01:04:13 +0800

    request_account template blade fix

--- app/Http/Controllers/mdd/dashboard/manageusercontroller.php
+++ app/Http/Controllers/mdd/dashboard/manageusercontroller.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\mdd\dashboard;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\mdd\RegisterModel;
 
 class manageusercontroller extends Controller
 {
     public function request_account() {
 
-        return view('mdd.pages.dashboard.manage_user.request_account_index');
+        $data = RegisterModel::all();
+        return view('mdd.pages.dashboard.manage_user.request_account_index',compact('data'));
     }
 }
